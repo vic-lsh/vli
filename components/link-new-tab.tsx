9f6e8cf467a5c688034acb506da3028fa397c680
vic-lee <viclee@nyu.edu>
2021-09-27 13:04:11 -0400

    feat: add optional classname prop to link cmp

--- components/link-new-tab.tsx
+++ components/link-new-tab.tsx
@@ -1,10 +1,16 @@
 /** Behaves the same as `<a></a>`, but opens in a new tab. */
-export const LinkToNewTab: React.FC<{ href: string }> = ({
+export const LinkToNewTab: React.FC<{ href: string; className?: string }> = ({
   children,
   href,
+  className,
 }) => {
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer">
+    <a
+      href={href}
+      className={className}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       {children}
     </a>
   );
