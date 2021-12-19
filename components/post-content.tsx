68913e1d82b7bed5d3cd857ef5fa723a4777febb
vic-shihang-li <viclee@nyu.edu>
2021-12-19 16:29:32 -0500

    feat: add spacing in blog content

--- components/post-content.tsx
+++ components/post-content.tsx
@@ -2,5 +2,14 @@ import ReactMarkdown from "react-markdown";
 import { Post } from "../libs/posts";
 
 export const PostContent: React.FC<{ post: Post }> = ({ post }) => {
-  return <ReactMarkdown>{post.content}</ReactMarkdown>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-4xl">{post.meta.title}</h2>
+      <div>{post.meta.dateStr}</div>
+
+      <div className="flex flex-col gap-2">
+        <ReactMarkdown>{post.content}</ReactMarkdown>
+      </div>
+    </div>
+  );
 };
