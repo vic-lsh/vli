2ac90d6d48a7d0af4dd8780f57d1f0fc88aefc8c
vic-shihang-li <viclee@nyu.edu>
2021-12-19 15:27:44 -0500

    refactor: reorganize read posts functionality

--- libs/posts/read-posts/read-posts.ts
+++ libs/posts/read-posts/read-posts.ts
@@ -10,6 +10,78 @@ interface ReadPostsConfig {
 
 const defaultPostsDirectory = resolvePostsDir();
 
+export function readPosts(
+  config: Partial<ReadPostsConfig> = {}
+): Promise<Post[]> {
+  const { directory = defaultPostsDirectory } = config;
+
+  return new Promise((resolve, reject) => {
+    fs.readdir(directory, async (err, postFileNames) => {
+      if (err) {
+        reject(`posts directory does not exist: ${directory}`);
+      }
+
+      return getPostsFromDirectory(postFileNames, directory)
+        .then((posts) => filterUnpublished(posts))
+        .then((posts) => sortPosts(posts))
+        .then((sortedPosts) => resolve(sortedPosts))
+        .catch((e) => reject(e));
+    });
+  });
+}
+
+async function getPostsFromDirectory(
+  postFileNames: string[],
+  directory: string
+): Promise<Post[]> {
+  return Promise.all(postFileNames.map((name) => getPost(directory, name)));
+}
+
+function filterUnpublished(posts: Post[]): Post[] {
+  return posts.filter((p) => p.meta.published);
+}
+
+function sortPosts(posts: Post[]): Post[] {
+  const coerceDateString = (s: string): Date => {
+    return new Date(Date.parse(s));
+  };
+
+  return posts.sort((post1, post2) => {
+    const post1Date = coerceDateString(post1.meta.dateStr);
+    const post2Date = coerceDateString(post2.meta.dateStr);
+
+    return post1Date.getTime() > post2Date.getTime() ? -1 : 1;
+  });
+}
+
+const getPost = (directory: string, fileName: string): Promise<Post> => {
+  return new Promise((resolve, reject) => {
+    const fullFilePath = path.join(directory, fileName);
+
+    fs.readFile(fullFilePath, "utf8", (err, fileData) => {
+      if (err) {
+        reject(`file at location '${fullFilePath}' does not exist`);
+      }
+
+      const { data, content } = matter(fileData);
+      const slug = getSlug(fileName);
+
+      try {
+        const meta = getPostMeta(data);
+        resolve({ slug, content, meta });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  });
+};
+
+const getPostMeta = (data: { [k: string]: any }): Post["meta"] => {
+  return validatePostMeta(extractPostMeta(data));
+};
+
+const getSlug = (mdFilename: string) => mdFilename.replace(/\.md$/, "");
+
 const extractPostMeta = (data: { [k: string]: any }) => {
   const extract = (propName: string, errorMsg: string) => {
     if (data[propName] != null) {
@@ -52,75 +124,3 @@ const validatePostMeta = (data: { [k: string]: any }): Post["meta"] => {
     published: coerceBoolean(data.published),
   };
 };
-
-const getPostMeta = (data: { [k: string]: any }): Post["meta"] => {
-  return validatePostMeta(extractPostMeta(data));
-};
-
-const getSlug = (mdFilename: string) => mdFilename.replace(/\.md$/, "");
-
-const getPost = (directory: string, fileName: string): Promise<Post> => {
-  return new Promise((resolve, reject) => {
-    const fullFilePath = path.join(directory, fileName);
-
-    fs.readFile(fullFilePath, "utf8", (err, fileData) => {
-      if (err) {
-        reject(`file at location '${fullFilePath}' does not exist`);
-      }
-
-      const { data, content } = matter(fileData);
-      const slug = getSlug(fileName);
-
-      try {
-        const meta = getPostMeta(data);
-        resolve({ slug, content, meta });
-      } catch (e) {
-        reject(e);
-      }
-    });
-  });
-};
-
-export const readPosts = (
-  config: Partial<ReadPostsConfig> = {}
-): Promise<Post[]> => {
-  const { directory = defaultPostsDirectory } = config;
-
-  return new Promise((resolve, reject) => {
-    fs.readdir(directory, async (err, postFileNames) => {
-      if (err) {
-        reject(`posts directory does not exist: ${directory}`);
-      }
-
-      return getPosts(postFileNames, directory)
-        .then((posts) => filterUnpublished(posts))
-        .then((posts) => sortPosts(posts))
-        .then((sortedPosts) => resolve(sortedPosts))
-        .catch((e) => reject(e));
-    });
-  });
-};
-
-async function getPosts(
-  postFileNames: string[],
-  directory: string
-): Promise<Post[]> {
-  return Promise.all(postFileNames.map((name) => getPost(directory, name)));
-}
-
-function filterUnpublished(posts: Post[]): Post[] {
-  return posts.filter((p) => p.meta.published);
-}
-
-function sortPosts(posts: Post[]): Post[] {
-  const coerceDateString = (s: string): Date => {
-    return new Date(Date.parse(s));
-  };
-
-  return posts.sort((post1, post2) => {
-    const post1Date = coerceDateString(post1.meta.dateStr);
-    const post2Date = coerceDateString(post2.meta.dateStr);
-
-    return post1Date.getTime() > post2Date.getTime() ? -1 : 1;
-  });
-}
