debee58595443e7c68d5abfd7255b212a15a8fce
vic-shihang-li <viclee@nyu.edu>
2021-12-20 17:11:52 -0500

    fix: revert changes to the slug file

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -5,6 +5,38 @@ import { PostContent } from "../../components/post-content";
 import { PostLayout } from "../../components/post-layout";
 import { getPostBySlug, Post, readPosts } from "../../libs/posts";
 
-export default function BlogPost() {
-  return <div></div>;
+interface StaticProps {
+  post: Post;
 }
+
+export default function BlogPost({ post }: StaticProps) {
+  return (
+    <PostLayout>
+      <PostContent post={post} />
+    </PostLayout>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const postSlug = typeof slug === "string" ? slug : slug.at(-1);
+  const post = await getPostBySlug(postSlug);
+
+  return {
+    props: { post },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await readPosts();
+
+  return {
+    paths: posts.map((post) => ({
+      params: {
+        slug: post.slug,
+      },
+    })),
+    fallback: false,
+  };
+};
