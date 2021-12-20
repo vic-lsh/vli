ae1793a675bf95b1c833517d1e035910a22b4389
vic-shihang-li <viclee@nyu.edu>
2021-12-20 13:38:49 -0500

    build: add docs to vercel script

--- vercel-checkout.sh
+++ vercel-checkout.sh
@@ -1,6 +1,15 @@
-git clone https://$GITHUB_ACCESS_TOKEN@github.com/vic-shihang-li/vli-blogs.git
+# This script pulls blog posts, which live on a private repository.
+#
+# The script pulls the blog repository's private content, and moves it into
+# the expected submodule location.
+# 
+# Dependencies:
+# 	GITHUB_ACCESS_TOKEN configured on vercel
+
 
 set -e
+
+git clone https://$GITHUB_ACCESS_TOKEN@github.com/vic-shihang-li/vli-blogs.git
 
 mv vli-blogs blogs/vli-blogs
 mv blogs/vli-blogs/* blogs/content
