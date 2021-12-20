bd546a2104186895956bcc6d08afe81391bbfa4d
vic-shihang-li <viclee@nyu.edu>
2021-12-20 13:35:11 -0500

    build: use simplified script

--- vercel-checkout.sh
+++ vercel-checkout.sh
@@ -1,56 +1,6 @@
-#!/bin/sh
+git clone https://$GITHUB_ACCESS_TOKEN@github.com/vic-shihang-li/vli-blogs.git
 
-# script to automate build process that pulls private git submodule.
-# adapted from: https://itsf.red/1337/vercel-private-submodules-workaround/
-
-# repo paths (supplied without the protocol prefix)
-MAIN_REPO=github.com/vic-shihang-li/vli.git
-# the reference of the submodule in .gitmodules (usually the path)
-SUBMODULE_PATH=blogs/content
-
-if [ "$VERCEL_GIT_COMMIT_SHA" == "" ]; then
-  echo "Error: VERCEL_GIT_COMMIT_SHA is empty"
-  exit 1
-fi
-if [ "$GITHUB_ACCESS_TOKEN" == "" ]; then
-  echo "Error: GITHUB_ACCESS_TOKEN is empty"
-  exit 1
-fi
-
-# stop script execution on error
 set -e
-# set up an empty temporary work directory
-rm -rf vercel-tmp || true
-mkdir vercel-tmp
-cd vercel-tmp
-# checkout the current commit
-git init
-git remote add origin https://$GITHUB_ACCESS_TOKEN@$MAIN_REPO
-git fetch --depth=1 origin $VERCEL_GIT_COMMIT_SHA
-git checkout $VERCEL_GIT_COMMIT_SHA
-# set the submodule repo paths to ones that vercel can access
-cat .gitmodules > .gitmodules.original
-cat .gitmodules | sed "s/github.com/$GITHUB_ACCESS_TOKEN@&/" > .gitmodules.tmp
-mv .gitmodules.tmp .gitmodules
-# checkout the submodule
-git submodule sync
-git submodule update --init --recursive 2>&1 | sed "s/$GITHUB_ACCESS_TOKEN/\*\*\*\*/"
 
-# move the submodule to where it should have been if vercel had supported submodules
-cd ..
-rm -rf vercel-tmp/$SUBMODULE_PATH/.git
-
-ls -l vercel-tmp/$SUBMODULE_PATH/
-
-mkdir -p $(dirname $SUBMODULE_PATH)
-mv vercel-tmp/$SUBMODULE_PATH/ $SUBMODULE_PATH
-
-# show contents of submodule path in logs
-ls -l $SUBMODULE_PATH
-ls -l blogs
-ls -l blogs/content
-ls -l blogs/content/content
-ls -l blogs/content/posts
-
-# clean up
-rm -rf vercel-tmp
+mv vli-blogs blogs/vli-blogs
+mv blogs/vli-blogs/* blogs/content
