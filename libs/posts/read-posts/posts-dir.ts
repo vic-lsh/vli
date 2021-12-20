b21d841e249c07154859ccde790e2a3f240477ee
vic-shihang-li <viclee@nyu.edu>
2021-12-20 17:07:13 -0500

    ci: fix cypress build environment

--- libs/posts/read-posts/posts-dir.ts
+++ libs/posts/read-posts/posts-dir.ts
@@ -12,7 +12,13 @@ export const makePostsFixturePath = (...subDirectory: string[]) => {
 
 export const resolvePostsDir = (): string => {
   const env = process.env.NODE_ENV;
-  if (env === "development" || env === "test") {
+
+  // APP_ENV is set to `test` in builds for tests, such as a build for e2e tests.
+  // NODE_ENV is never set to `test`. See:
+  // https://github.com/vercel/next.js/issues/9123#issuecomment-543784086
+  const appEnv = process.env.APP_ENV;
+
+  if (env === "development" || appEnv === "test") {
     return makePostsFixturePath("functional");
   } else {
     return makeAbsPath("blogs", "content", "posts");
