801bae8df259324c5d75ee5e811f7dbbc441bfd5
vic-shihang-li <viclee@nyu.edu>
2021-12-19 22:13:39 -0500

    fix: attempt to use a very simple default export

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -5,36 +5,6 @@ import { PostContent } from "../../components/post-content";
 import { PostLayout } from "../../components/post-layout";
 import { getPostBySlug, Post, readPosts } from "../../libs/posts";
 
-interface StaticProps {
-  post: Post;
+export default function BlogPost() {
+  return <div></div>;
 }
-
-function BlogPost({ post }: StaticProps) {
-  return <PostLayout></PostLayout>;
-}
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { slug } = params;
-
-  const postSlug = typeof slug === "string" ? slug : slug.at(-1);
-  const post = await getPostBySlug(postSlug);
-
-  return {
-    props: { post },
-  };
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const posts = await readPosts();
-
-  return {
-    paths: posts.map((post) => ({
-      params: {
-        slug: post.slug,
-      },
-    })),
-    fallback: false,
-  };
-};
-
-export default BlogPost;
