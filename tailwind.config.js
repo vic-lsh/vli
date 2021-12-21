d88638ab0ae437162bb7aa6708dd6855beff5360
vic-shihang-li <viclee@nyu.edu>
2021-12-21 14:19:38 -0500

    build: upgrade to tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    // specify source files in which tailwind is used
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         "html-blue": "#0000ff",
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
