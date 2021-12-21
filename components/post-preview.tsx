130ec9a4295bee1160e1cca9e97a7b614ef286e0
vic-shihang-li <viclee@nyu.edu>
2021-12-21 14:39:33 -0500

    feat: tweak dark mode styling for blog page

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -16,14 +16,16 @@ export const PostPreview: React.FC<Props> = ({ post }) => {
 
   return (
     <Link href="/blogs/[slug]" as={`/blogs/${post.slug}`} passHref>
-      <header data-cy="post-preview" className="py-3 cursor-pointer">
+      <header data-cy="post-preview" className="py-6 cursor-pointer">
         <div className="flex items-center justify-between">
-          <h2 data-cy="title" className="text-xl font-medium">
+          <h2 data-cy="title" className="text-2xl font-medium py-2">
             <a className="hover:underline cursor-pointer">{title}</a>
           </h2>
           <span data-cy="date">{dateStr}</span>
         </div>
-        <span data-cy="summary">{summary}</span>
+        <span data-cy="summary" className="dark:text-gray-300">
+          {summary}
+        </span>
       </header>
     </Link>
   );
