c7c838d5efb3c520d5275345e0164b03f1f6b874
vic-shihang-li <viclee@nyu.edu>
2021-12-20 23:31:22 -0500

    feat: nav tab styling tweak

--- components/nav-tab.tsx
+++ components/nav-tab.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const NavTab: React.FC<{ href: string }> = ({ children, href }) => {
   return (
     <Link href={href} passHref>
-      <span className="hover:bg-gray-200 transition-all px-3 py-2 rounded-md underline">
+      <span className="hover:border-black transition-all px-3 py-2 rounded-md underline cursor-pointer">
         {children}
       </span>
     </Link>
