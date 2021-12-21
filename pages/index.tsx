064f9a2fdb6a55966c89be10e25de06a66430c0a
vic-shihang-li <viclee@nyu.edu>
2021-12-21 15:21:13 -0500

    fix: update front page spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,7 +92,7 @@ export default function Home() {
           </NavTab>
         </NavBar>
 
-        <div className="mt-10 md:mt-32 md:pt-0 flex flex-col md:justify-center items-center">
+        <div className="py-10 md:pt-0 md:mt-32 flex flex-col md:justify-center items-center">
           <NameHeader>Shihang (Vic) Li</NameHeader>
           <ContactInfo />
           <PersonalIntro />
