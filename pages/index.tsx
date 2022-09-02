5d858805e559ed88c805e7e9d725a4fd7d977789
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-09-02 16:00:17 -0400

    update personal intro statement

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,12 @@ const ContactInfoContainer: React.FC = ({ children }) => (
   <span className="flex items-center justify-center gap-2">{children}</span>
 );
 
+const UrlLink: React.FC<{ href: String }> = ({ children, href }) => (
+  <a className="underline" href={href}>
+    {children}
+  </a>
+);
+
 const ContactInfo = () => {
   return (
     <div className="m-10 gap-2 flex flex-col justify-center items-start">
@@ -52,12 +58,21 @@ const ContactInfo = () => {
 const PersonalIntro = () => {
   return (
     <div className="flex flex-col gap-4 max-w-2xl">
-      <p className="text-justify">{`
-  Hi! I'm Vic, a Master's Computer Science student at Brown.
-  I'm interested in making multi-threaded programs easier to write and reason
-  about. I'm also interested in how programming language design
-  influences the way we think about programming.
-`}</p>
+      <p className="text-justify">
+        {`
+  Hi! I'm Vic, a second-year Master's Computer Science student at Brown.
+`}
+        I'm currently part of Professor{" "}
+        <UrlLink href="https://cs.brown.edu/people/malte/">
+          Malte Schwarzkopf
+        </UrlLink>
+        's <UrlLink href="https://etos.cs.brown.edu">ETOS group</UrlLink>
+      </p>
+      <p classname="text-justify">
+        {`I'm interested in making it easier to write safe and fast concurrent 
+  programs. I also like to think about how programming language design
+  influences the way we think about programming.`}
+      </p>
       <p className="text-justify">
         {`In my spare time, I enjoy hiking, snowboarding, urban photography,
   and playing the piano.`}
