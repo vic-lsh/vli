5e84d7444343a48be8e72882b8ee15270bab95ee
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2023-10-23 22:29:33 -0700

    tweak bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import { NavTab } from "../components/nav-tab";
 
 const GH_URL = "https://www.github.com/vic-shihang-li";
 
-const EMAIL_ADDR = "shli [at] cs [dot] washington [dot] edu";
+const EMAIL_ADDR = "shli [at] cs [dot] uw [dot] edu";
 
 const FEATURE_FLAGS = {
   enable_blogs: false,
@@ -87,7 +87,7 @@ const TitleArea = () => (
 const PersonalIntroArea = () => {
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-justify">
+      <p>
         {`Hello there! I'm Vic, a first-year Computer Science Ph.D. student at
         University of Washington. `}
 
@@ -110,13 +110,13 @@ const PersonalIntroArea = () => {
         <UrlLink href="https://etos.cs.brown.edu">ETOS group</UrlLink>
         {` at Brown.`}
       </p>
-      <p className="text-justify">
+      <p>
         <span>
           {`These days, I'm thinking about how to design high-performing
           systems to best serve emerging datacenter computing trends. `}
         </span>
         <span>
-          {`My latest work focus on improving datacenter resource utilization
+          {`My latest work focuses on improving datacenter resource utilization
           with HW/SW co-design, resource disaggregation, and smart scheduling.`}
         </span>
         {/*
